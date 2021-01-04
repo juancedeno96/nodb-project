62fc6b99e5ac5dc12add58c3d0b55b6f8c5b9bcd
juancedeno96 <cedenojj7@gmail.com>
2021-01-04 16:45:11 -0700

    made more progess

---
+++ src/server/controller/favoriteAlbum.js
@@ -0,0 +1,31 @@
+const favoriteAlbums = [];
+let id = 1
+
+module.exports = {
+    getFavoriteAlbums: (req, res) => {
+        res.status(200).send(favoriteAlbums)
+    },
+
+    selectFavorite: (req, res) => {
+        const {albums} = req.body;
+        albums.id = id;
+        id++;
+        favoriteAlbums.push(albums)
+        res.status(200).send(favoriteAlbums)
+    },
+
+    giveRating: (req, res) => {
+        const {id} = req.params
+        const {rating} = req.body
+        const albums = favoriteAlbums.find(element => element.id === +id);
+        albums.rating = rating
+        res.status(200).send(favoriteAlbums)
+    },
+
+    deleteAlbum: (req, res) => {
+        const {id} = req.params;
+        const index = favoriteAlbums.findIndex(element => element.id=== +id);
+        favoriteAlbums.splice(index, 1)
+        res.status(200).send(favoriteAlbums)
+    },
+}
